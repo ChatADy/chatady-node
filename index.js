b5145eac7d4419883f83ddfce9e66a3affe4cef7
Jernej Pregelj <jernej.pregelj@gmail.com>
2026-02-23 11:45:17 +0100

    v4.0.0, removed ips, consolidated to one ad type

--- index.js
+++ index.js
@@ -12,12 +12,11 @@ const ChatADy = (publisherId, key, inputClientOptions = {}) => {
   }, inputClientOptions)
 
   return {
-    newChat: async (chatId, chatterId, chatterIp, entry, ad = undefined) => {
+    newChat: async (chatId, chatterId, entry, ad = undefined) => {
       return new Promise((resolve, reject) => {
         const postData = JSON.stringify({
           entry,
-          ad,
-          chatterIp
+          ad
         });
         const options = {
           hostname,
